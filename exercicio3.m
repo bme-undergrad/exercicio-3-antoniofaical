cdc758c463eedb9686094a0b7e5e1fb8e016895c
Antonio Elias Faical Junior <142061917+antoniofaical@users.noreply.github.com>
2025-12-06 17:37:22 -0300

    Add cell growth model and calculation for mu
    Implement mathematical model for cell growth using differential equations and linear regression.

--- exercicio3.m
+++ exercicio3.m
@@ -5,11 +5,44 @@ function [mu] = exercicio3(t, NC)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+% Garantindo que t vai começar em 0
 
+t = t - t(1);
+
+% A equação diferencial fornecida é:
+% dX/dt = μ * X
+% onde:
+% X  = número de células
+% μ  = taxa de crescimento específico
+
+% A solução dessa equação é:
+% X(t) = X0 * exp(μ * t)
+
+% Aplicando ln:
+% ln(X) = ln(X0) + μ * t
+
+% Podemos transformar essa equação em um modelo linear:
+% y = a + μ * t
+% onde:
+% a = ln(X0)
+
+y_poly = log(NC);
+
+% Com isso, podemos usar o polyfit para descobrir μ e ln(X0), de tal forma que:
+
+% y_poly = p(1)*t + p(2)
+%
+% onde:
+% p(1) = μ
+% p(2) = ln(X0)
+
+p = polyfit(t, y_poly, 1);
+
+% Por fim, o μ calculado é o primeiro elemento de p:
+mu_calculado = p(1);
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = 0;
+mu = mu_calculado
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
